bfaf74e5ece87f5aae3c450072105ab82ebe574b
sankar gurusamy <sankar.gurusamy@gmail.com>
2015-11-08 17:20:53 -0700

    Initial boiler plate code for course web api project

---
+++ CourseWebApi/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CourseWebApi.Controllers
+{
+    public class CourseController : ApiController
+    {
+        // POST api/values
+        public String Post([FromBody]List<string> inputs)
+        {
+            if (inputs != null)
+            {
+
+                return String.Join(",", inputs);
+            }
+            else
+            {
+                return "Nothing received";
+            }
+        }
+    }
+}
